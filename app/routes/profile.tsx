87881c4938b4d734cfc1b660054fad29346657da
kakkaned <kakkaned.bua@rmutto.ac.th>
2024-11-30 12:34:02 +0700

    week3

---
+++ app/routes/profile.tsx
@@ -0,0 +1,26 @@
+export default function MyProfile() {
+    return (
+        <><>
+            <div className="p-5 mt-10 mb-5 ml-5 mr-2 border-2 border-black rounded">
+            <h1 className="text-xl font-bold">kakkaend</h1>
+            <strong className="text-lg">026640491806-9</strong>
+            </div>
+        </>
+        <MyEducation /></>
+    );  
+
+}
+
+function MyEducation () {
+    return (
+         <div className="p-5 m-5 border-2 border-yellow 500">
+            <ul>
+                <li>Information Technology, RMUTTO, 2567-present</li>
+                <li>xxxxxxxxxxxxxxxxxxxx</li>
+                <li>xxxxxxxxxxxxxxx</li>
+            </ul>
+         </div>
+    );  
+
+}
+
