174505b39e7521d1fb0e0cc451d42baf03f03af0
kakkaned <kakkaned.bua@rmutto.ac.th>
2024-12-14 12:12:08 +0700

    week5

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,26 +1,23 @@
 export default function MyProfile() {
     return (
-        <><>
+        <>
             <div className="p-5 mt-10 mb-5 ml-5 mr-2 border-2 border-black rounded">
-            <h1 className="text-xl font-bold">kakkaend</h1>
-            <strong className="text-lg">026640491806-9</strong>
+                <h1 className="text-xl font-bold">kakkaend</h1>
+                <strong className="text-lg">026640491806-9</strong>
             </div>
+            <MyEducation /> {/* เรียกใช้คอมโพเนนต์ MyEducation */}
         </>
-        <MyEducation /></>
     );  
-
 }
 
-function MyEducation () {
+function MyEducation() {
     return (
-         <div className="p-5 m-5 border-2 border-yellow 500">
+        <div className="p-5 mt-10 mb-5 ml-5 mr-2 border-2 border-black rounded">
             <ul>
                 <li>Information Technology, RMUTTO, 2567-present</li>
                 <li>xxxxxxxxxxxxxxxxxxxx</li>
                 <li>xxxxxxxxxxxxxxx</li>
             </ul>
-         </div>
-    );  
-
+        </div>
+    );
 }
-
---
+++ app/routes/toDoLists.tsx
@@ -0,0 +1,20 @@
+export const toDoItem = [
+    {
+        id: 1,
+        title: "กิจกรรม การนอนที่ถูกวิธี",
+        created: "2024-12-14 09:46:00",
+        completed: true
+    },
+    {
+        id: 2,
+        title: "กิจกรรม การกินที่ถูกวิธี",
+        created: "2024-12-14 10:00:00",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "กิจกรรม การใชซีวิต(❁´◡`❁)",
+        created: "2024-12-14 10:30:00",
+        completed: true
+    }
+];
